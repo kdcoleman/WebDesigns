4259c296b7889c6ba5546f1876a4bb85ecd64626
Kayla D. Coleman <kdcolem2@ncsu.edu>
2019-10-08 17:21:49 -0400

    Added comments to make code more clear

--- email.php
+++ email.php
@@ -1,6 +1,6 @@
-<?php 
+<?php
 if(isset($_POST['submit'])){
-    $to = "kay.coleman807@gmail.com"; // this is your Email address
+    $to = "sender@email.com"; // this is your Email address
     $from = $_POST['email']; // this is the sender's Email address
     $first_name = $_POST['first_name'];
     $last_name = $_POST['last_name'];
